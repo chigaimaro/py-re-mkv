36fd5f83d1d26dd9b9b6787046a3e1e66d83547f
chigaimaro <marcusjohnson@thestreamoftime.com>
2020-03-15 02:39:21 -0400

    Add initialize_queue function

---
+++ mkvlib/remfiles.py
@@ -0,0 +1,2 @@
+mkv_extensions = ("*.WEBM", "*.MPG", "*.MP2", "*.MPEG", "*.MPE", "*.MPV", "*.MP4", "*.M4P", "*.M4V", "*.AVI",
+                  "*.WMV", "*.MOV", "*.QT", "*.FLV", "*.SWF", "*.AVCHD", "*.MKV")
--- mkvlib/remmedia.py
+++ mkvlib/remmedia.py
@@ -1,6 +1,6 @@
 # Contains media processing related functions
 import logging
-from mkvlib import remset
+from mkvlib import remset, remfiles
 
 log = logging.getLogger(__name__)
 
@@ -8,12 +8,17 @@ log = logging.getLogger(__name__)
 class RemSession:
     def __init__(self):
         self.settings = remset.AvSettings()
-        self.queue = None
+        self.queue = sorted(self.initialize_queue())
         self.current_file_map = None
-        # TODO - Add media related variables
 
     def __next__(self):
         pass
 
     def initialize_queue(self):
-        self.settings = None
+        queue = []
+        for ext in remfiles.mkv_extensions:
+            new_ext = str('**/' + ext)
+            queue.extend(self.settings.folders['input-folder'].glob(new_ext))
+        return queue
+
+    # TODO Create File map function
